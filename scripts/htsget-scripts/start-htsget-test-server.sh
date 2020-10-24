9a82a89d6f7e62d55e41e912d9476231fa8cee9b
Chris Norman <cnorman@broadinstitute.org>
2020-10-24 09:24:09 -0400

    Update htsget script to run locally.

--- scripts/htsget-scripts/start-htsget-test-server.sh
+++ scripts/htsget-scripts/start-htsget-test-server.sh
@@ -6,7 +6,7 @@ DOCKER_NAME='ga4gh/htsget-refserver'
 SERVER_VERSION='1.1.0'
 DOCKER_COORDINATE=${DOCKER_NAME}:${SERVER_VERSION}
 
-WORKING_DIR=/home/travis/build/samtools/htsjdk
+WORKING_DIR=/Users/cnorman/projects/htsjdk
 docker pull ${DOCKER_COORDINATE}
 docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://127.0.0.1:3000 \
       -v $WORKING_DIR/src/test/resources/htsjdk/samtools/BAMFileIndexTest/:/data \
